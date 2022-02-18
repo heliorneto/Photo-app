67d2802314c34e4dbe47fa101fd1e55f5be32023
Hélio Neto <helioneto29@hotmail.com>
2022-02-18 08:16:00 -0300

    Criando componente photo.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  photos = [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg",
+      description: "Leao"
+    },
+    {
+      url: "https://ichef.bbci.co.uk/news/640/cpsprodpb/F767/production/_103953336_de27.jpg",
+      description: "Leoa"
+    }
+  ]
 }
